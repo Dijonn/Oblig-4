53c854a944dfa049f094644bbcdd1b056a61d341
hans andersen <hans.andersen02@gmail.com>
2022-03-14 14:08:44 +0100

    Update Pasient.java

--- Pasient.java
+++ Pasient.java
@@ -1,13 +1,1 @@
-public class Pasient {
-    protected String navn;
-    protected String foedselsnummmer;
-    static protected int idteller; 
-    protected int id;
-
-    public Pasient(String navn, String foedselsnummer){
-        this.navn = navn;
-        this.foedselsnummmer = foedselsnummer;
-        idteller +=1;
-        id = idteller;
-    }
-}
+public class pasient{}
