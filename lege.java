fced660e72a0d2b818e9c42077830c511dc2a071
hans andersen <hans.andersen02@gmail.com>
2022-03-15 10:50:03 +0100

    D3
    la inn metoder for å legge til resepter

--- lege.java
+++ lege.java
@@ -26,6 +26,28 @@ class lege implements Comparable<lege> {
             System.out.println("er dette leggtil eller settinn?????");
         }
     }
+
+    public hvitResept skrivHvitResept (Legemiddel legemiddel, pasient pasient, int reit)throws UlovligUtskrift{
+        hvitResept hvit = new hvitResept(this, reit, legemiddel, pasient);
+        utskrevneResepter.leggTil(hvit);
+        return hvit;
+    }
+
+    public MilitaerResept skrivMilResept (Legemiddel legemiddel, pasient pasient) throws UlovligUtskrift {
+        MilitaerResept mil = new MilitaerResept(this, legemiddel, pasient);
+        utskrevneResepter.leggTil(mil);
+        return mil;
+    }
+    public pResept skrivPResept (Legemiddel legemiddel, pasient pasient, int reit) throws UlovligUtskrift {
+        pResept p = new pResept(this, reit, legemiddel, pasient);
+        utskrevneResepter.leggTil(p);
+        return p;
+    }
+    public blaaResept skrivBlaaResept (Legemiddel legemiddel, pasient pasient, int reit) throws UlovligUtskrift{
+        blaaResept blaa = new blaaResept(this, reit, legemiddel, pasient);
+        utskrevneResepter.leggTil(blaa);
+        return blaa;
+    }
 }
 
 
