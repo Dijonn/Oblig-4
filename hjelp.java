834ffeae7772357caf2886a67d0294a43523084d
hans andersen <hans.andersen02@gmail.com>
2022-03-15 12:32:52 +0100

    Update hjelp.java

--- hjelp.java
+++ hjelp.java
@@ -1,6 +1,6 @@
 class Test{
     public static void main(String[] args) {
-        Koe<String> ny = new Koe<>(); 
+        Koe<String> ny = new Koe<>();
         ny.leggTil("x");
         ny.leggTil("1");
         for(String x: ny){
