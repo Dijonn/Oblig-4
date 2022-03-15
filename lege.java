52a7ea5200100b24617d909fd19a95625bf52652
nicolaitran <91554358+nicolaitran@users.noreply.github.com>
2022-03-15 09:45:04 +0100

    Update lege.java

--- lege.java
+++ lege.java
@@ -8,6 +8,10 @@ class lege implements Comperable<Lege>{
         return "Navn: " + navn;
     }
     
+    public String hentNavn(){
+        return navn;
+    }
+    
     public int compareTo(Lege x){
         return x.hentNavn().compareTo(this.navn);
     }
