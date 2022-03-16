d01d158ac6dc476008b842004ad6d4cfc4ef2833
Dijonn <dionkumnova@hotmail.com>
2022-03-16 13:48:10 +0100

    hans du suger

--- pResept.java
+++ pResept.java
@@ -3,13 +3,18 @@
 class pResept extends hvitResept{
     pResept( lege ansvarligLege, int reit, Legemiddel legemiddel, pasient pasient){
         super(ansvarligLege, reit, legemiddel, pasient);   
-        if(legemiddel.hentPris() -108 > 0){
-            this.Pris =legemiddel.hentPris() -108;
-        } else{
-            this.Pris = 0;
-        }
+        
     }
     public String toString(){
         return super.toString();
+    }
+
+    public int prisAaBetale(){
+        if(Legemiddel.hentPris() -108 > 0){
+            this.Pris =Legemiddel.hentPris() -108;
+        } else{
+            this.Pris = 0;
+        }
+        return this.Pris;
     }
 }
