615fab54529454b415c55a741e5adfee71e5687d
hans andersen <hans.andersen02@gmail.com>
2022-03-16 11:37:22 +0100

    hei

--- Legesystem.java
+++ Legesystem.java
@@ -6,19 +6,46 @@ public class Legesystem {
 
     public void lesFraFil(String fil) throws FileNotFoundException{
         File text = new File(fil);
-        Scanner leser = new Scanner(fil);
+        Scanner leser = new Scanner(text);
         String linje;
-        while(leser.hasNextLine()){
-            while(leser.nextLine() != "# Legemidler (navn,type,pris,virkestoff,[styrke])"){
-                if(leser.nextLine() != "# Pasienter (navn, fnr)"){
+        int hashtagTeller = 0;
+        
+        while(leser.hasNextLine()){   //gå gjennom alle linjene i filen 
+            linje = leser.nextLine();
+            hashtagTeller ++;
+            
+            while(! linje.contains("#") ){ // gå gjennom alle linjer knyttet til pasient
+                
+                if(hashtagTeller == 2){
+                    System.out.println("pasienter");
+                }
+                else if(hashtagTeller == 3){
+                    System.out.println("legemidler");
+                }
+                else if(hashtagTeller == 4){
+                    System.out.println("lege");
+                }
+                else if(hashtagTeller == 5){
+                    System.out.println("resept");
+                }
+
+                System.out.println(linje);
+                
+
+                if(leser.hasNextLine()){
                     linje = leser.nextLine();
                     String deler[] = linje.split(",");
-                    String navn = deler[0];
-                    int foedselsnummer = Integer.parseInt(deler[1]);
-                    System.out.println(navn + foedselsnummer);
+
+                }
+                else{
+                    break;
                 }
             }
         }
         leser.close();
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        Legesystem test = new Legesystem();
+        test.lesFraFil("legedata.txt");
     }
 }
