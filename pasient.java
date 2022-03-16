8819cca324fffff0ff3223e163645760a835a7c8
nicolaitran <91554358+nicolaitran@users.noreply.github.com>
2022-03-16 09:10:38 +0100

    Update pasient.java

--- pasient.java
+++ pasient.java
@@ -1,1 +1,16 @@
-public class pasient{}
+public class Pasient {
+    protected String navn;
+    protected String foedselsnummmer;
+    static protected int idteller; 
+    protected int id;
+
+    public Pasient(String navn, String foedselsnummer){
+        this.navn = navn;
+        this.foedselsnummmer = foedselsnummer;
+        idteller +=1;
+        id = idteller;
+    }
+
+    public String toString(){
+        return ("Navn: " + this.navn + "\n"+ "Foedselsnummer: " + this.foedselsnummmer + "\n" + "ID: " + this.id);
+    }
