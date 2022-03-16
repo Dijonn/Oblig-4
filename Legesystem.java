176bae1f6dd1e4776ad92ef59b7dbd7e85be576e
hans andersen <hans.andersen02@gmail.com>
2022-03-16 12:47:14 +0100

    update main

--- Legesystem.java
+++ Legesystem.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class Legesystem {
 
+    IndeksertListe<pasient> pasienter = new IndeksertListe<>();
+    IndeksertListe<Legemiddel> legemiddel = new IndeksertListe<>();
+    IndeksertListe<lege> lege = new IndeksertListe<>();
+    IndeksertListe<Resept> resept = new IndeksertListe<>();
+
     public void lesFraFil(String fil) throws FileNotFoundException{
         File text = new File(fil);
         Scanner leser = new Scanner(text);
         String linje;
         int hashtagTeller = 0;
+        
+
         
         while(leser.hasNextLine()){   //gå gjennom alle linjene i filen 
             linje = leser.nextLine();
@@ -16,12 +23,32 @@ public class Legesystem {
             
             while(! linje.contains("#") ){ // gå gjennom alle linjer knyttet til pasient
                 
+                String deler[] = linje.split(",");
+
                 if(hashtagTeller == 2){
-                    //System.out.println("pasienter");
+                    //lager ny pasient og legger til i pasientlenketliste
+                    pasient p = new pasient(deler[0], deler[1]);
+                    pasienter.leggTil(p);
                     
                 }
+
                 else if(hashtagTeller == 3){
-                    //System.out.println("legemidler");
+                    //lager nytt legemiddel og legger dette i legemiddellenketliste;
+                    String narkotisk = "narkotisk";
+                    String vanlig = "vanlig";
+                    String vanedannende = "vanedannende";
+
+                    if(deler[1].compareTo(narkotisk) == 0 ){ //hvis type er vanlig
+                        Narkotisk ny = new Narkotisk(deler[0], Integer.parseInt(deler[2]), Integer.parseInt(deler[3]), Integer.parseInt(deler[4]));
+                        legemiddel.leggTil(ny);
+                    }
+                    else if(deler[1].compareTo(vanlig) == 0 ){ //hvis type er vanlig
+                        
+                    }
+                    else{  //ett alternativ igjen, vanedannende. 
+                        
+                    }
+
                 }
                 else if(hashtagTeller == 4){
                     //System.out.println("lege");
@@ -33,8 +60,6 @@ public class Legesystem {
 
                 if(leser.hasNextLine()){
                     linje = leser.nextLine();
-                    String deler[] = linje.split(",");
-
                 }
 
                 else{
@@ -43,11 +68,15 @@ public class Legesystem {
 
             }
         }
+        for(pasient x: pasienter){
+            System.out.println(x);
+        }
         leser.close();
-        
+
     }
     public static void main(String[] args) throws FileNotFoundException {
         Legesystem test = new Legesystem();
         test.lesFraFil("legedata.txt");
+        
     }
 }
