29a87635a487e647064983c5e0c48e16eb8c0f8a
Dijonn <91554314+Dijonn@users.noreply.github.com>
2022-03-17 11:22:59 +0100

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
-public class GUI {
+public class GUI extends Legesystem{
     public static void main(String[] args) {
+
         String svar = "";
         Scanner input = new Scanner(System.in);
-        System.out.println(
+                
+        while(svar != "q" ){
+            
+            System.out.println(
                 "(S)krive ut en fullstendig oversikt over pasienter, leger, legemidler og resepter\n" +
                 "(O)pprette og legge til nye elementer i systemet \n" +
                 "(B)ruke en gitt resept fra listen til en pasient \n" +
                 "(P)rint ut forskjellige former for statistikk \n" +
                 "(L)age ny fil og skrive alle data til fil \n"+
                 "(Q)uit");
-                
-            
-        while(svar != "q" ){
-            
+
             System.out.print("Skriv inn oensket kommando: ");  // bare pynt
             svar = input.next();
             System.out.print("Dette skjedde: ");  //bare pynt
@@ -25,13 +26,66 @@ public class GUI {
             }
 
             else if(svar.toLowerCase().compareTo("q") == 0){
-                System.out.println("hade");
+                System.out.println("Hade!");
                 break;
             }
 
             else if(svar.toLowerCase().compareTo("o") == 0){
                 //ikke lagt inn funksjon
                 System.out.println("Opprette og legge til nye elementer i systemet (deloppgave E4).");
+                System.out.println("Ønsker du å opprette: \n"+ 
+                "(L)ege \n" +
+                "(P)asient \n"+
+                "(R)esept \n"+
+                "(LE)gemiddel \n"+
+                "(Q)uit til hovedmeny");
+
+                while(svar != "Q"){
+
+                    System.out.print("Skriv inn oensket kommando: ");  // bare pynt
+                    svar = input.next();
+
+                    if(svar.toLowerCase().compareTo("l") == 0){
+                        System.out.print("(S)pesialist eller (V)anlig: ");
+                        svar = input.next();
+                        if(svar.toLowerCase().compareTo("s") == 0){
+                            System.out.println("Oppgi navn;");
+                            String svar1 = input.next(); 
+                            System.out.println("Oppgi kontrollid");
+                            String svar2 = input.next();
+                            spesialist nySpes = new spesialist(svar1, svar2);
+                            leger.leggTil(nySpes);
+                        }
+                        else if(svar.toLowerCase().compareTo("v") == 0){
+                            System.out.println("Oppgi navn;");
+                            String svar1 = input.next();
+                            lege nyLege = new lege(svar1);
+                            leger.leggTil(nyLege);
+                        }
+
+                        else{
+                            System.out.println("Ugyldig"); break;
+                        }
+                    }
+
+                    else if(svar.toLowerCase().compareTo("p") == 0){
+
+                    }
+
+                    else if(svar.toLowerCase().compareTo("r") == 0){
+
+                    }
+
+                    else if(svar.toLowerCase().compareTo("le") == 0){
+
+                    }
+
+                    else if(svar.toLowerCase().compareTo("q") == 0){
+                        System.out.println("taper"); break;
+
+                    }
+                    else{System.out.println("ulovlig input");}
+                }
             }
             
             else if(svar.toLowerCase().compareTo("b") == 0){
@@ -55,5 +109,9 @@ public class GUI {
             System.out.println("\n");
         }
         input.close();
+        for(lege x:leger){
+            System.out.println(x);
+        }
     }   
 }
+
